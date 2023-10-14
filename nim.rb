b0c6b704da5a84520ed0c7f7dfb5d0839a08537a
AJ Wilson <ajwilson@AJs-MacBook-Air.local>
2023-10-13 20:46:34 -0700

    user turn

--- nim.rb
+++ nim.rb
@@ -40,8 +40,7 @@ class Nim
     def possible_boards
         temp = []
         possible_boards = []
-        (0...@board.length).each do |i|
-            pile = @board[i]
+        @board.each_with_index do |pile, i|
             (1..pile).each do |j|
                 temp = @board.dup
                 temp[i] = @board[i] - j
@@ -50,12 +49,38 @@ class Nim
         end
         return possible_boards
     end
+
+    def update(new_board)
+        @board = new_board
+        display
+    end
 end
 
 class User
-    def turn
+    def turn(game)
         print ("Enter move [?, ?, ?, ?]: ")
         input = gets.chomp
-        return input.split(',').map(&:to_i)
+        user_move = input.split(',').map(&:to_i)
+        if user_move.class == Array && user_move.length == 4
+            if game.possible_boards.include?(user_move)
+                return user_move # user move is valid
+            else
+                puts "Error: Invalid move, try again."
+                turn
+            end
+        else
+            puts "Error: Enter your move as 4 numbers separated by commas ex. 1, 3, 5, 7"
+            turn
+        end
     end
 end
+
+game = Nim.new
+bot = NimPlayer.new
+user = User.new
+
+game.display
+puts "User move:"
+game.update(user.turn(game))
+puts "Computer move:"
+game.update(bot.logic(game))
